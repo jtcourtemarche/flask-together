2c689c32cc5b207332a38495e7c08e78d00f0516
Unknown <jacob.courtemarche@gmail.com>
2018-09-21 19:05:50 -0400

    Prevent UnboundLocalError from occurring if there are no users

--- sockets.py
+++ sockets.py
@@ -2,6 +2,7 @@
 
 import re
 import time
+import json
 from flask import request, session
 from flask_login import current_user
 from flask_socketio import emit, join_room
@@ -24,7 +25,7 @@ def get_active_users():
 
     return active_users
 
-@socketio.on('joined')
+@socketio.on('user:joined')
 def handle_connect():
     # New connection handler
     clients.append(request.sid)
@@ -36,44 +37,44 @@ def handle_connect():
 
     active_users = get_active_users()
 
-    emit('new-user', {
+    emit('server:new-user', {
         'active_users': active_users
     }, broadcast=True)
 
     history_schema = models.HistorySchema()
 
     try:
-        most_recent = models.History.query.order_by(
-            models.History.date).all()[-1]
+        most_recent = models.History.query.order_by(db.text('-id')).first()
         most_recent = history_schema.dump(most_recent).data
         most_recent_username = models.User.query.get(
             most_recent['user']).username
     except:
         most_recent = None
+        most_recent_username = None
 
     history_schema = models.HistorySchema(many=True)
-    history = models.History.query.order_by('date').all()
+    history = models.History.query.order_by('date').limit(20).all()
     history = history_schema.dump(history).data
 
-    emit('new-user-sync', {
+    emit('server:sync', {
         'most_recent': most_recent,
         'most_recent_username': most_recent_username,
-        'history': history,
+        'history': history[:20], 
         'sid': request.sid,
     }, room=clients[-1])
 
 
-@socketio.on('init-preload')
+@socketio.on('user:init-preload')
 def init_preload():
     if [x for x in clients if x != request.sid] != []:
-        emit('request-data', {
+        emit('server:request-data', {
             'sid': request.sid,
         }, broadcast=True, include_self=False)
 
 
-@socketio.on('preload-info')
+@socketio.on('user:preload-info')
 def preload(data):
-    emit('preload', data, room=data['sid'])
+    emit('server:preload', data, room=data['sid'])
 
 
 @socketio.on('disconnect', namespace='/')
@@ -89,7 +90,7 @@ def handle_dc():
     pipe.set(current_user.username, '')
     pipe.execute()
 
-    emit('user-disconnected', {'username': current_user.username,
+    emit('server:disconnected', {'username': current_user.username,
                                'active_users': active_users}, broadcast=True)
 
     cin = clients.index(request.sid)
@@ -97,17 +98,17 @@ def handle_dc():
 
 # Play / Pause
 
-@socketio.on('client-play')
+@socketio.on('user:play')
 def play(data):
-    emit('server-play', data["time"], broadcast=True)
+    emit('server:play', data["time"], broadcast=True)
 
 
-@socketio.on('client-pause')
+@socketio.on('user:pause')
 def pause(data):
-    emit('server-pause', data["time"], broadcast=True)
+    emit('server:pause', data["time"], broadcast=True)
 
 
-@socketio.on('client-play-new')
+@socketio.on('user:play-new')
 def play_new(data):
     # Extract video id from yt url
     yt_re = r'(https?://)?(www\.)?youtube\.(com|nl|ca)/watch\?v=([-\w]+)'
@@ -132,63 +133,68 @@ def play_new(data):
         db.session.add(h)
         db.session.commit()
         history_schema = models.HistorySchema(many=True)
-        history = models.History.query.all()
+        history = models.History.query.limit(20).all()
         history = history_schema.dump(history).data
 
-        emit('server-play-new', {
-            'id': h.video_id,
-            'title': h.video_title,
+        emit('server:play-new', {
+            'id': items['id'],
+            'title': items['snippet']['title'],
             'history': history, 
             'user': user.username,
+            'content': content,
         }, broadcast=True)
-
-        # Scrobbling
-        pipe.get(current_user.username)
-        get_cache = pipe.execute()
-
-        if get_cache != [b'']:
-            # Send scrobble to API then clear from cache
-            fm.scrobble(current_user.username)
-            pipe.set(current_user.username, '')
-            pipe.execute()
-        
-        if len(items['snippet']['title'].split(' - ')) == 2:
-            # Check if song
-            title = items['snippet']['title'].split(' - ')
-            artist = title[0]
-            name = re.sub(r'\([^)]*\)', '', title[1])
-
-            emit('server-play-new-artist', {
-                'artist': fm.get_artist(artist),
-            }, broadcast=True)
-
-            # Handle scrobbling after playing video
-
-            if current_user.lastfm_connected():
-                duration = content['contentDetails']['duration']
-                fm.update_now_playing(artist, name, current_user, duration)
-        else:
-            # Denote that nothing is being scrobbled anymore
-            pipe.set(current_user.username, '')
-            pipe.execute()
 
     elif '/channel/' in data['url']:
         results = utils.check_channel_yt(data['url'])
-        emit('server-serve-list', results, room=request.sid)
+        emit('server:serve-list', results, room=request.sid)
     else:
         results = utils.search_yt(data['url'])
-        emit('server-serve-list', results, room=request.sid)
+        emit('server:serve-list', results, room=request.sid)
 
-@socketio.on('client-rate')
+@socketio.on('user:play-callback')
+def play_new_handler(d):
+    # Scrobbling
+    pipe.get(current_user.username)
+    get_cache = pipe.execute()
+
+    d = json.loads(d['data'])
+
+    if get_cache != [b'']:
+        # Send scrobble to API then clear from cache
+        fm.scrobble(current_user.username)
+        pipe.set(current_user.username, '')
+        pipe.execute()
+    
+    if len(d['title'].split(' - ')) == 2:
+        # Check if song
+        title = d['title'].split(' - ')
+        artist = title[0]
+        name = re.sub(r'\([^)]*\)', '', title[1])
+
+        emit('server:play-new-artist', {
+            'artist': fm.get_artist(artist),
+        }, broadcast=True)
+
+        # Handle scrobbling after playing video
+
+        if current_user.lastfm_connected():
+            duration = d['content']['contentDetails']['duration']
+            fm.update_now_playing(artist, name, current_user, duration)
+    else:
+        # Denote that nothing is being scrobbled anymore
+        pipe.set(current_user.username, '')
+        pipe.execute()    
+
+@socketio.on('user:rate')
 def handle_rate(data):
-    emit('server-rate', data["rate"], broadcast=True)
+    emit('server:rate', data["rate"], broadcast=True)
 
-@socketio.on('client-skip')
+@socketio.on('user:skip')
 def handle_skip(data):
-    emit('server-skip', data["time"], broadcast=True)
+    emit('server:skip', data["time"], broadcast=True)
 
 # Error handling
 
 @socketio.on_error()
 def error_handler(e):
-    print(e)
+    print(e.args, type(e).__name__)
