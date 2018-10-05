ff38c229709b59f085a0583410f2f3fb780dd3c9
jtcourtemarche <jacob.courtemarche@gmail.com>
2018-10-04 23:03:05 -0400

    Working app manager

--- manager.py
+++ manager.py
@@ -5,9 +5,17 @@ class Manager:
         self.db = db
         self.models = models
 
+        self.commands = {
+            'init_db':self.init_db,
+            'wipe_db':self.wipe_db,
+            'add_user':self.add_user,
+            'del_user':self.del_user,
+            'list_users':self.list_users,
+        }
+
         print("""
             Welcome to the Locke manager
-            Access the following commands using app.mgr.*()
+            Access the following commands:
 
             * init_db => initializes database
             * wipe_db => wipes database
@@ -17,6 +25,16 @@ class Manager:
                 - params: username
             * list_users => list all registered users
         """)
+
+    def run(self):
+        print('Enter a command\n')
+        io = input('>>> ')
+
+        try:
+            self.commands[io]()
+        except Exception as e:
+            print('Error ', e)
+            exit()
 
     def printc(self, msg):
         print(msg)
@@ -32,7 +50,10 @@ class Manager:
         self.db.session.commit()
         self.printc('Wiped database')
 
-    def add_user(self, username, password):
+    def add_user(self):
+        username = input('Username: ')
+        password = input('Password: ')
+
         u = self.models.User(username=username)
         u.setpass(password)
 
@@ -55,20 +76,25 @@ class Manager:
 
         self.printc(f'Added users: {users}')
 
-    def del_user(self, username):
-        if username:
-            u = self.models.User.query.filter_by(username=username).first()
+    def del_user(self):
+        username = input('Username: ')
 
-            if u != None:
-                self.db.session.delete(u)
-                self.db.session.commit()
-                self.printc(f'Deleted user: {u}')
-            else:
-                print(f'Could not find username {username}')
+        u = self.models.User.query.filter_by(username=username).first()
+
+        if u != None:
+            self.db.session.delete(u)
+            self.db.session.commit()
+            self.printc(f'Deleted user: {u}')
         else:
-            print('Invalid parameters!')
+            print(f'Could not find username {username}')
 
     def list_users(self):
         users = self.models.User.query.all()
         users = [user.username for user in users]
         self.printc(', '.join(users))
+
+if __name__ == '__main__':
+    from app import app, models
+    from extensions import db
+    mgr = Manager(db, models)
+    mgr.run()
